5d8d0b09e52ed087216440a8e0d2812b5b641654
beichenhpy <beichenhpy@outlook.com>
2021-02-05 14:00:18 +0800

    1、完成适配器模式--添加使用场景解释

--- src/AdapterPattern/V200ToV5Adapter.java
+++ src/AdapterPattern/V200ToV5Adapter.java
@@ -3,7 +3,7 @@ package AdapterPattern;
 /**
  * @author beichenhpy
  * @version 1.0
- * @description TODO 转换器-->相当于是为了满足Phone条件而创建的实现类
+ * @description TODO 转换器-->相当于是为了满足Phone条件而创建的实现类（针对旧方法如果使用了A_interface，一旦业务变动需要使用B_interface 那么就需要新建此适配器，实现A_interface 引入 B_interface）
  * @since 2021/2/5 13:41
  */
 public class V200ToV5Adapter implements V5Power{
